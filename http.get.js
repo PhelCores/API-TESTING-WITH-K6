1c7b6101fd8aa4e94b29ff9a2f3d5dd7212a99e7
Phelipe <phelipecores@gmail.com>
2024-07-03 13:42:16 +0200

    Initial commit

---
+++ http.get.js
@@ -0,0 +1,44 @@
+
+
+import http from 'k6/http'
+import { check } from 'k6'
+
+
+
+export default function ( ) {
+
+    let res = http.get('https://test-api.k6.io/public/crocodiles/')
+
+
+    const crocodile = res.json()
+    const crocoID = crocodile[0].id
+    const crocoName = crocodile[0].name
+    const crocoSex = crocodile[0].sex
+    const crocoAge = crocodile[0].age
+    const croco_date_of_birth = crocodile[0].date_of_birth
+
+    res = http.get(`https://test-api.k6.io/public/crocodiles/${crocoID}/`)
+
+// console.log(res.json().id)
+// console.log(res.json().name)
+// console.log(res.json().sex)
+// console.log(res.json().age)
+// console.log(res.json().date_of_birth)
+
+
+//Assertions 
+
+    check(res, {
+        'STAUTS IS 200' :(r) => r.status === 200,
+        'CROC ID IS: 0000000': (r) => r.json().id === crocoID,
+        'CROC NAME IS: 0000000': (r) => r.json().name === crocoName,
+        'CROC SEX IS: 0000000': (r) => r.json().sex === crocoSex,
+        'CROC AGE IS: 0000000': (r) => r.json().age === crocoAge,
+        'CROC DATE OF BIRTH: 0000000': (r) => r.json().date_of_birth === croco_date_of_birth,
+        
+
+
+
+    })
+  
+}
---
+++ http.post.js
@@ -0,0 +1,64 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export default function () {
+    // Generate unique credentials for each test iteration
+    const credentials = {
+        username: "User_" + Date.now(),
+        password: "Secret_" + Date.now()
+    };
+
+    // Register a new user in the data base
+    http.post(
+        'https://test-api.k6.io/user/register/',
+        JSON.stringify(credentials),
+        {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    );
+
+    // Log in and retrieve access token
+    let res = http.post(
+        'https://test-api.k6.io/auth/token/login/',
+        JSON.stringify({
+            username: credentials.username,
+            password: credentials.password
+        }),
+        {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    );
+    const accessToken = res.json().access;
+    console.log(accessToken);
+
+    // Make a GET request using the access token
+    http.get(
+        'https://test-api.k6.io/my/crocodiles/',
+        {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        }
+    );
+
+
+     //Create a new Resource (Crocodile)
+    http.post('https://test-api.k6.io/my/crocodiles/',
+        JSON.stringify({
+            name: 'NewCrocodile',
+            sex: 'M',
+            date_of_birth:'1992-12-09'
+        }),
+         {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+                
+            }
+        }
+    )
+}
